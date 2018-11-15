a759b799cd1d0b5ea3a51ac67db521e1575cc51d
hoshiimo28 <ty21sanvu@gmail.com>
2018-11-15 10:57:25 +0900

    Add files via upload
    human_position_preditoin
    
    【概要】
    動画から10フレーム分を学習し、10フレーム後（可変）の位置を推定する。
    
    【入力】
    case１．Raspberry Piがカメラで撮影し、sambaサーバにフレームごとに映像を出力された画像を入力とする
    case２．PCのWebカメラでキャプチャした映像を入力とする
    
    【出力】
    Case1.キャプチャした画像に現在の重心位置と推定の重心位置を重畳する
    Case2.重心位置と推定の重心位置を重畳する
    
    【フロー】
    以下を無限ループ
    ・指定フォルダを監視
    ・ファイル（画像）追加されたらOpenposeで人体検出＆関節位置をJson化
    ・データを整形し、機械学習にかける
    ・結果を出力
    
    【準備】
    ・「EstimateTrainig」で作成した機械学習のモデルと重みを「model」フォルダにおく
    ・環境に合わせてソース内で各種ディレクトリパスを指定
    ・RaspberryPi使用かWebカメラ使用かはコメントアウトで選択
    
    【備考】
    Visual Studio 2015(VS以外の環境だと、カレントディレクトリをソース上で指定しないといけないかも) 
    pyhton 3.5
    tensorflow1.4.0

---
+++ human_position_preditoin/human_position_preditoin/human_position_preditoin.py
@@ -0,0 +1,204 @@
+import json
+import numpy as np
+import cv2
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import os
+import time
+from glob import glob
+from keras.layers import Dense, Activation
+from keras import optimizers
+from keras import regularizers
+from keras.models import Sequential, model_from_json
+import matplotlib.pyplot as plt
+import shutil
+import subprocess
+
+
+
+# jsonファイルが置かれるディレクトリ#samba上の画像が置かれるディレクトリ
+saved_jason_dir = (r'C:\Users\km65725\Documents\Visual Studio 2015\Projects\jsondir')
+
+# 一度読み込んだ画像の移動先
+# img_to_dir = (r'')
+
+# openpose.exeのパス
+# video_to_json_openposepath = (r'./~~~~/OpenPoseDemo.exe')
+
+# USBカメラ用Openposeパス
+# 
+usbcam_openpose_path = (r'/openpose.exe')
+
+# モデルと重みの場所と名前
+f_model = './model'
+weights_filename = 'openpose_model_weights.hdf5'
+model_filename = 'openpose_model.json'
+
+# 黒背景画像
+img = cv2.imread('original.jpg')
+#img = np.zeros_like(img_)
+
+# ファイルの更新を監視し、変更があったときに動作するクラス
+class ChangeHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        #print('%s' % filename)
+
+        #######RPiのときのみ有効########
+        # openposeを動かすバッチ。Sambaにおかれた画像をJson化する
+        #subprocess.run((video_to_json_openposepath,--video,filename,--write_keypoint_json,saved_jason_dir))
+        #######RPiのときここまで########
+
+        # 画像からのjsonデータを10フレームのかたまりにして入力するデータセットを作成
+        openposedataset, current_gravity = Json2Nparray(GetOldestJson())
+
+        # データセットを入力し、10frame後の動きを予測
+        if openposedataset.size == 540:
+            classes, future_gravity = EstimateAction.estimate(openposedataset)
+            
+            # current_gravity,可視化
+            
+            ##### WEBカメラのとき
+            img = cv2.imread('original.jpg') # Webカメラのときはこっち
+            
+            #### RPiのとき
+            # img = cv2.imread(filename) # RPiのときJsonファイルを作った元画像を表示
+            
+            cv2.circle(img,(int(current_gravity[0]), int(current_gravity[1])),20,(255,255,0),thickness = -1)
+            cv2.circle(img,(int(future_gravity[0]), int(future_gravity[1])),20,(255,0,255),thickness = -1)
+            #cv2.destroyAllWindows()
+
+            cv2.imshow('nacho', img)
+            cv2.waitKey(1)
+            #time.sleep(1)
+
+
+        ########RPiのときのみ有効##########
+        # 一度読み込んだ画像を移動する
+        #shutil.move(filename,img_to_dir)
+
+
+    def on_modified(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        print('%sを変更しました' % filename)
+
+    def on_deleted(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        print('%sを削除しました' % filename)
+
+
+# DeepLearningにより、0.5s後の状態を推定
+class EstimateAction:
+
+    @staticmethod
+    def estimate(openposedataset):
+        openposedataset = openposedataset.reshape((1,540))
+        epochs = 20
+        batch_size = 100
+        # モデルの読み込み
+        json_string = open(os.path.join(f_model, model_filename)).read()
+        model = model_from_json(json_string)
+
+        model.summary()
+
+        model.compile(optimizer='adam',loss='mse', metrics=['accuracy'])
+
+        # 重みの読み込み
+        model.load_weights(os.path.join(f_model,weights_filename))
+
+        # print(model.summary())
+
+        classes = model.predict(openposedataset,epochs,batch_size)
+
+        print(classes)
+
+        future_gravity = EstimateAction.XYPosition(classes)
+        print(future_gravity)
+
+        # classes:予測した体54点のデータ、future_gravity:体の重心位置
+        return classes, future_gravity
+
+
+    def XYPosition(classes):
+        ar = np.reshape(classes,(-1,3))
+        ar_sum = ar.sum(axis = 0)
+        future_gravity = (ar_sum[0]/ar.shape[0],ar_sum[1]/ar.shape[0])
+        return future_gravity
+
+
+def GetOldestJson():
+    target = os.path.join(saved_jason_dir, '*')
+    files = [(f, os.path.getmtime(f)) for f in glob(target)]
+    latest_modified_file_path = sorted(files, key=lambda files:files[1])
+    print(latest_modified_file_path[0])#[0]:oldest
+    print(latest_modified_file_path[-1])#[-1]:latest
+    return latest_modified_file_path[-10:]
+
+
+def Json2Nparray(file_list_path):
+    trainingdata = np.zeros((0,540))
+
+    if len(file_list_path) < 10:
+        # ファイルが１０個たまるまではトレーニングデータ生成を行わない
+        return np.zeros(0), np.zeros(0)
+
+    elif len(file_list_path) > 9:
+        for i,file_list in enumerate(file_list_path):
+            # 最新の１０ファイル分だけを予測データとする
+            if i <10:
+                with open(file_list[0]) as f:
+                    data = json.load(f)
+                trainingdata = np.append(trainingdata,np.array([data['people'][0]['pose_keypoints_2d']]))
+                print('trainingdata.size=',trainingdata.size)
+            if i == 9:
+                ar = np.reshape(data['people'][0]['pose_keypoints_2d'],(-1,3))
+                ar_sum = ar.sum(axis = 0)
+                current_gragity = (ar_sum[0]/ar.shape[0],ar_sum[1]/ar.shape[0])
+                return trainingdata, current_gragity
+  
+
+if __name__ == '__main__':
+    print("Reday")
+
+    #######################################
+    # Rpiから画像を受け取る場合
+    #######################################
+    while 1:
+        event_handler = ChangeHandler()
+        
+        observer = Observer()
+        # 今はjsonファイルを格納するsaved_jason_dirが監視対象だが、本番ではimg_dirになる
+        observer.schedule(event_handler, saved_jason_dir, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+    ####RPiの場合ここまで#####################
+
+
+    ########################################
+    ## USBカメラで処理する場合
+    ########################################
+    ## USBカメラ起動&Jsonファイル保存
+    #subprocess.run((usbcam_openpose_path,--write_keypoint_json,saved_jason_dir))
+    ## Observer起動
+    #    while 1:
+    #    event_handler = ChangeHandler()
+    #    
+    #    observer = Observer()
+    #    # jsonファイルを格納するsaved_jason_dirを指定
+    #    observer.schedule(event_handler, saved_jason_dir, recursive=True)
+    #    observer.start()
+    #    try:
+    #        while True:
+    #            time.sleep(0.1)
+    #    except KeyboardInterrupt:
+    #        observer.stop()
+    #   observer.join()
+    ##USBカメラの場合ここまで##################
